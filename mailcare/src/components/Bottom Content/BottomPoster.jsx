861c12db3a4922a4ec737a5d645d106c43f26759
princepatel7405 <prince.m.7405@gmail.com>
2022-11-13 16:48:57 +0530

    final update

--- mailcare/src/components/Bottom Content/BottomPoster.jsx
+++ mailcare/src/components/Bottom Content/BottomPoster.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Box, Flex, Text,Spacer ,Button,Image} from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
 function BottomPoster() {
   return (
     <Box bg="blue.900">
@@ -13,7 +14,10 @@ function BottomPoster() {
             Create Your Free Account
           </Text>
           <Text color="white"  fontSize="6xl" fontFamily="Poppins">In 3 Clicks</Text>
+          <NavLink to="/sign-up">
+
           <Button color="white" mb="80px" bg="green.300" fontSize="14px" p="22px" mt="20px" >Sign Up Now</Button>
+          </NavLink>
         </Box>
         <Spacer/>
         <Image src="https://mailtrap.io/wp-content/uploads/2021/01/img__cta.svg" h="300px"/>
